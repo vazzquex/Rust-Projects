2349ec4926a0653b9f446debf76ecf3c3ef7394f
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-06-05 20:24:44 -0300

    finish v1.0 cal rust

--- calculator-rust/src/main.rs
+++ calculator-rust/src/main.rs
@@ -10,12 +10,49 @@ fn main() {
     let dato_oper: String = oper.trim().to_string();
 
     println!("Ingrese primer numero: ");
-    let mut num1 = String::new();
+    let mut num1: String  = String::new();
 
     io::stdin().read_line(&mut num1).expect("Error al leer primer numero");
 
+    println!("Ingrese segundo numero: ");
+    let mut num2: String = String::new();
 
-    println!("Operacion es {}, y el numero es {}", dato_oper, num1);
+    io::stdin().read_line(&mut num2).expect("Error al leer segundo numero");
+
+    let num1: i32 = num1.trim().parse().expect("Error al convertir primer numero");
+    let num2: i32 = num2.trim().parse().expect("Error al convertir Segundo numero");
 
 
+    if dato_oper == "+" {
+        let result: i32 = num1 + num2;
+        println!("------------------------------------");
+        println!("El resultado de la operacion es: {}", result);
+    }
+
+    match dato_oper.as_str() {  
+        "+" => {
+            let result: i32 = num1 + num2;
+            println!("------------------------------------");
+            println!("El resultado de la operacion es: {}", result);
+        }
+        "-" => {
+            let result: i32 = num1 - num2;
+            println!("------------------------------------");
+            println!("El resultado de la operacion es: {}", result);
+        }
+        "/" => {
+            let result: i32 = num1 / num2;
+            println!("------------------------------------");
+            println!("El resultado de la operacion es: {}", result);
+        }
+        "*" => {
+            let result: i32 = num1 * num2;
+            println!("------------------------------------");
+            println!("El resultado de la operacion es: {}", result);
+        }
+        
+        _ => {
+            println!("Operacion no valida")
+        }
+    }
 }
