ee6a4295a37e961ca92ade2f423488e3f945c93f
Santiago Vázquez <108772623+vazzquex@users.noreply.github.com>
2023-06-05 18:52:40 -0300

    Delete calculator-rust/target directory

--- calculator-rust/target/debug/calculator-rust.d
+++
@@ -1,1 +0,0 @@
-/home/svazquez/Desktop/Rust-Tauri/calculator-rust/target/debug/calculator-rust: /home/svazquez/Desktop/Rust-Tauri/calculator-rust/src/main.rs
--- calculator-rust/target/debug/deps/calculator_rust-3ba11f9c50bcb053.d
+++
@@ -1,5 +0,0 @@
-/home/svazquez/Desktop/Rust-Tauri/calculator-rust/target/debug/deps/calculator_rust-3ba11f9c50bcb053: src/main.rs
-
-/home/svazquez/Desktop/Rust-Tauri/calculator-rust/target/debug/deps/calculator_rust-3ba11f9c50bcb053.d: src/main.rs
-
-src/main.rs:
